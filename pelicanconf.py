419090608b0c27a0d113696b21085ac560a7723a
Laure Daumal <daumal_l@epita.fr>
2016-10-24 21:04:51 +0300

    add basic files & medius theme

---
+++ pelicanconf.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Synergie'
+SITENAME = 'Journées Synergiques'
+SITEURL = ''
+DESCRIPTION = 'Ateliers d\'initiation aux métiers de l\'informatique'
+
+PATH = 'content'
+THEME = 'themes/medius'
+
+LOCALE = ("fr_FR")
+TIMEZONE = 'Europe/Paris'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = 10
+RELATIVE_URLS = True
+
+# Content
+USE_FOLDER_AS_CATEGORY = False
